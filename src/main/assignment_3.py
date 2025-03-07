a624aa3b2a0ee557a929c2bba99444fa39d5f8da
HyrumVonn <uberhimer@gmail.com>
2025-03-07 17:01:58 -0500

    Euler

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,10 +1,41 @@
 #Hyrum VonNiederhausern
 
-def BaseFunction(t, y):
+def f(t, y):
     return t - y * y
 
+# #example from slides
+# def f(t, y):
+#     return y - t * t + 1
+
 def Euler(rangeLower, rangeUpper, iterations, initailPoint):
-    return 1.24
+    if(rangeLower > rangeUpper):
+        temp = rangeUpper
+        rangeUpper = rangeLower
+        rangeLower = temp
+    
+    if(rangeLower == rangeUpper):
+        print(f"Error, range must have non-equal start and end points (current {rangeLower} and {rangeUpper})")
+        return
+    
+    if(iterations <= 0):
+        print(f"Error, Iterations must be positive. Currently {iterations}")
+        return
+    
+    y = []
+    x = []
+    #x.append(rangeLower)
+    stepsize = (rangeUpper - rangeLower) / iterations
+
+    for i in range(iterations + 1):
+        x.append(rangeLower + stepsize * i)
+        
+        if(i == 0):
+            y.append(initailPoint)
+            continue
+
+        y.append(y[i - 1] + stepsize * f(x[i-1], y[i-1])) 
+
+    return y[iterations]
 
 def RungeKutta(rangeLower, rangeUpper, iterations, initailPoint):
     return 1.25
@@ -14,6 +45,8 @@ rangeL = 0
 iterations = 10
 initialPoint = 1
 
-Euler(rangeL, rangeU, iterations, initialPoint)
+print(Euler(rangeL, rangeU, iterations, initialPoint))
 
-RungeKutta(rangeL, rangeU, iterations, initialPoint)
+print()
+
+print(RungeKutta(rangeL, rangeU, iterations, initialPoint))
--- src/test/test_assignment_3.py
+++ src/test/test_assignment_3.py
@@ -10,6 +10,10 @@
 def f(t, y):
     return t - y * y
 
+# #example from slides
+# def f(t, y):
+#     return y - t * t + 1
+
 def Euler(rangeLower, rangeUpper, iterations, initailPoint):
     if(rangeLower > rangeUpper):
         temp = rangeUpper
@@ -24,8 +28,23 @@ def Euler(rangeLower, rangeUpper, iterations, initailPoint):
         print(f"Error, Iterations must be positive. Currently {iterations}")
         return
     
+    y = []
+    x = []
+    #x.append(rangeLower)
     stepsize = (rangeUpper - rangeLower) / iterations
 
+    for i in range(iterations + 1):
+        x.append(rangeLower + stepsize * i)
+        
+        if(i == 0):
+            y.append(initailPoint)
+            continue
+
+        y.append(y[i - 1] + stepsize * f(x[i-1], y[i-1])) 
+
+    #print(y)
+    #print(x)
 
 
 Euler(0, 2, 10, 1)
+# Euler(0,2,10,.5)
