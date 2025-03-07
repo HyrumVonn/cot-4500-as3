42caaf1598e524d085e3da960fe9b4c05cbdb36a
HyrumVonn <uberhimer@gmail.com>
2025-03-07 17:51:22 -0500

    RungeKutta

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -37,16 +37,55 @@ def Euler(rangeLower, rangeUpper, iterations, initailPoint):
 
     return y[iterations]
 
-def RungeKutta(rangeLower, rangeUpper, iterations, initailPoint):
-    return 1.25
 
+def RungeKutta(rangeLower, rangeUpper, iterations, initailPoint):
+    if(rangeLower > rangeUpper):
+        temp = rangeUpper
+        rangeUpper = rangeLower
+        rangeLower = temp
+    
+    if(rangeLower == rangeUpper):
+        print(f"Error, range must have non-equal start and end points (current {rangeLower} and {rangeUpper})")
+        return
+    
+    if(iterations <= 0):
+        print(f"Error, Iterations must be positive. Currently {iterations}")
+        return
+    
+    y = []
+    x = []
+    #x.append(rangeLower)
+    stepsize = (rangeUpper - rangeLower) / iterations
+    stepsize2 = stepsize / 2
+
+    for i in range(iterations + 1):
+        x.append(rangeLower + stepsize * i)
+        
+        if(i == 0):
+            y.append(initailPoint)
+            continue
+
+        k1 = stepsize * f(x[i-1], y[i-1])
+        k2 = stepsize * f(x[i-1] + stepsize2, y[i-1] + k1 / 2)
+        k3 = stepsize * f(x[i-1] + stepsize2, y[i-1] + k2 / 2)
+        k4 = stepsize * f(x[i], y[i-1] + k3)
+
+        y.append(y[i - 1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6) 
+
+    return y[iterations]
+
+#initialize, both will be using the same values
 rangeU = 2
 rangeL = 0
 iterations = 10
 initialPoint = 1
 
+#euler
 print(Euler(rangeL, rangeU, iterations, initialPoint))
 
 print()
 
+#RungeKutta
 print(RungeKutta(rangeL, rangeU, iterations, initialPoint))
+
+print()
--- src/test/test_assignment_3.py
+++ src/test/test_assignment_3.py
@@ -42,9 +42,49 @@ def Euler(rangeLower, rangeUpper, iterations, initailPoint):
 
         y.append(y[i - 1] + stepsize * f(x[i-1], y[i-1])) 
 
-    #print(y)
-    #print(x)
+    print(y)
+    print(x)
 
+def RungeKutta(rangeLower, rangeUpper, iterations, initailPoint):
+    if(rangeLower > rangeUpper):
+        temp = rangeUpper
+        rangeUpper = rangeLower
+        rangeLower = temp
+    
+    if(rangeLower == rangeUpper):
+        print(f"Error, range must have non-equal start and end points (current {rangeLower} and {rangeUpper})")
+        return
+    
+    if(iterations <= 0):
+        print(f"Error, Iterations must be positive. Currently {iterations}")
+        return
+    
+    y = []
+    x = []
+    #x.append(rangeLower)
+    stepsize = (rangeUpper - rangeLower) / iterations
+    stepsize2 = stepsize / 2
 
-Euler(0, 2, 10, 1)
+    for i in range(iterations + 1):
+        x.append(rangeLower + stepsize * i)
+        
+        if(i == 0):
+            y.append(initailPoint)
+            continue
+
+        k1 = stepsize * f(x[i-1], y[i-1])
+        k2 = stepsize * f(x[i-1] + stepsize2, y[i-1] + k1 / 2)
+        k3 = stepsize * f(x[i-1] + stepsize2, y[i-1] + k2 / 2)
+        k4 = stepsize * f(x[i], y[i-1] + k3)
+
+        y.append(y[i - 1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6) 
+
+    print(y)
+    print(x)
+
+    return y[iterations]
+
+#Euler(0, 2, 10, 1)
 # Euler(0,2,10,.5)
+
+RungeKutta(0,2,10,1)
